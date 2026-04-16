c875c7cf2a341648cc20f887cd9a72ea553cb9e8
usr-undeleted <zephimhere@gmail.com>
2026-04-16 19:04:15 -0300

    basic functionality for scan

---
+++ README.md
@@ -0,0 +1,20 @@
+# cutie Core UTIlity Environment
+Undeleted's life long goal to write their own core utils, following their own design principles, all in C alongside assembly (once i actually learn it-).  
+
+### Will strive for:
+- Reusing as many utilities in the code as possible. Move commonly used functions to a common lib.
+- Only type of fancy you'll see is colors. Maybe.
+- Only using what you need.
+- Keep It Simple, Stupid, I guess...
+- Bin naming should have its name instantly obvious to the user. "create something", "scan something, a directory"
+- If the shell can do it, don't bother.
+
+## Current implementations:
+- scan directories (scan); replace both ls and find, use --recursive|-r for find's functionality, otherwise, ls by default
+
+## Planned:
+- printf wrapper (print)
+- query search (search)
+- file reading (show)
+- file/dir removal (rm); applies to directories and regular files
+- file/dir creation (create); create directories with "name/". should support recursion, with flag or not (create folder/file)
--- src/scan.c
+++ src/scan.c
@@ -1,28 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <string.h>
 #include <unistd.h>
 
-// find contents in dir, allocate string with entries
-// int listFiles (char *dir) {}
-
-void mallocErr() {
-    printf("Malloc has failed to run. Aborting command.\n");
-    exit(1);
-}
+// array with pointers to structs
 
 int main (int argc, char *argv[]) {
-    if (argc == 1) {
-        char *buf;
-        long size = pathconf(".", _PC_PATH_MAX);
+    char *dir;
+    DIR *dirStream;
+    struct dirent *currentFile;
 
-        if ((buf = (char *)malloc((size_t)size)) != NULL) {
-            char *pwd = getcwd(buf, (size_t)size);
-            printf("%s\n", pwd);
-        } else {
-            mallocErr();
+    if (argc == 1) { // get cwd
+        dirStream = opendir(".");
+    } else {
+        dirStream = opendir(argv[1]);
+    }
+
+    if (dirStream == NULL) {
+        perror("Directory couldn't be opened");
+        return 1;
+    }
+
+    // print directory
+    int quant = 0;
+    while ((currentFile = readdir(dirStream)) != NULL) {
+
+        if (quant == 7) {
+            printf("\n");
+            quant = 0;
         }
-        // listFiles(pwd);
+
+        if (currentFile->d_name[0] != '.') {
+            if (currentFile->d_type == DT_DIR) {
+                printf("\033[34m%s\033[0m   ",currentFile->d_name);
+            } else {
+                printf("%s\033[0m  ",currentFile->d_name);
+            }
+
+            quant++;
+        }
+    }
+    if (quant != 7) {
+        printf("\n");
+    }
+
+    if (closedir(dirStream) == -1) {
+        perror("Couldn't close directory");
+        return -1;
     }
     return 0;
 }
