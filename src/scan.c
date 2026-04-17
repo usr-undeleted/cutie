87c36d585f3aaa6e81b98004cdd34620b3ba1b85
usr-undeleted <zephimhere@gmail.com>
2026-04-17 13:23:10 -0300

    Update scan.c

--- src/scan.c
+++ src/scan.c
@@ -7,17 +7,6 @@
 #include <unistd.h>
 #include "cutie-common.h"
 
-void helpMenu() {
-    printf("scan command basic usage:\n"
-        "   scan <flags> <dirs>\n"
-        "scan will search trough all directories you specificy.\n"
-        "flags:\n"
-        "   -h or --help: show this menu.\n\n"
-        "scan is part of the cutie project hosted under https://github.com/usr-undeleted/cutie licensed under the GPLv3 license."
-    );
-    exit(0);
-}
-
 // will contain a whole directory
 struct entry {
     char *name;
