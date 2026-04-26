b797e3536544ff394333955b2426d81d8ed3b1b9
usr-undeleted <zephimhere@gmail.com>
2026-04-26 14:21:43 -0300

    support for multiple queries with each having their own color

--- src/query.c
+++ src/query.c
@@ -1,5 +1,9 @@
 #include "cutie-common.h"
+#include <stdlib.h>
 #include <unistd.h>
+
+// case sensitive (1) or not (0)
+unsigned int beSensitive = 1;
 
 void helpMenu(char *invocation) {
     printf("\e[1m%s\e[0m command basic usage:\n"
@@ -8,6 +12,7 @@ void helpMenu(char *invocation) {
         "\e[1m%s\e[0m will search stdin and look for a match of your pattern to stdin. you can feed it stdin trough a pipe, for example.\n\n"
         "flags:\n"
         "   \e[1m-h\e[0m or \e[1m--help\e[0m: show this menu.\n"
+        "   \e[1m-i\e[0m or \e[1m--ignore-case\e[0m: disable case sensitive searching.\n\n"
         "\e[2;3m%s is part of the cutie project hosted under https://github.com/usr-undeleted/cutie licensed under the GPLv3 license.\e[0m\n",
         invocation, invocation, invocation, invocation
     );
@@ -25,10 +30,12 @@ int main (int argc, char *argv[]) {
 
     // manage flags
     char charFlags[] = {
-        'h'
+        'h',
+        'i'
     };
     char *stringFlags[] = {
-        "--help"
+        "--help",
+        "--ignore-case"
     };
     int charLen = sizeof(charFlags) / sizeof(charFlags[0]);
     int stringLen = sizeof(stringFlags) / sizeof(stringFlags[0]);
@@ -49,19 +56,24 @@ int main (int argc, char *argv[]) {
         return 2;
     }
 
-    // find what the user wants to query
-    unsigned int queryPos = -1;
-    for (int i = 1; i < argc; i++) {
-        if (argv[i][0] != '-') {
-            queryPos = i;
-            break;
+    // find all queries to be matched and their lens
+    int allQueries[argc];
+    unsigned int noQuery = 1;
+    size_t queriesLen = 0;
+    for (int i = 0; i < argc; i++) {
+        allQueries[i] = -1;
+        queriesLen++;
+
+        if (i != 0 && argv[i][0] != '-') {
+            allQueries[i] = strlen(argv[i]);
+            noQuery = 0;
         }
     }
-    if (queryPos == -1) {
+
+    if (noQuery) {
         printf("No query specified. See '%s -h' or '%s --help' for instructions.\n", argv[0], argv[0]);
         return 2;
     }
-    size_t queryLen = strlen(argv[queryPos]);
 
     unsigned int onlyFail = 1;
     unsigned int hadMatch = 0;
@@ -73,21 +85,34 @@ int main (int argc, char *argv[]) {
     while ((len = getline(&line, &cap, stdin)) != -1) {
         lastEnd = 0;
         hadMatch = 0;
-
-        // go trough all of line to find a matching char
+        // walk trough stdin
         for (int i = 0; i < len; i++) {
-            if (line[i] == argv[queryPos][0]) {
 
-                // compare
-                if (!strncmp(line + i, argv[queryPos], queryLen)) {
-                    // we have a match!
-                    printf("%.*s", (int)(i - lastEnd), line + lastEnd); // anything before the match
-                    printf("\e[31;1m%.*s\e[0m", (int)queryLen, line + i); // the actual query
-                    lastEnd = i + queryLen; // set when to start printing again
-                    i += queryLen - 1; // remove the match from the printing
-                    onlyFail = 0;
-                    hadMatch = 1;
+            // lets us pick the first longest query
+            int bestIdx = -1;
+            size_t bestLen = 0;
+
+            // walk trough all queries provided
+            for (int j = 0; j < queriesLen; j++) {
+                if (line[i] != argv[j][0] || allQueries[j] == -1) continue;
+
+                // if theres a match, we set the query to be shown as
+                // the longest one
+                if (!strncmp(line + i, argv[j], allQueries[j])) {
+                    if (allQueries[j] > bestLen) {
+                        bestLen = allQueries[j];
+                        bestIdx = j;
+                    }
                 }
+            }
+
+            if (bestIdx != -1) {
+                printf("%.*s", (int)(i - lastEnd), line + lastEnd);
+                printf("\e[3%d;1m%.*s\e[0m", bestIdx % 7, (int)bestLen, line + i);
+                lastEnd = i + bestLen;
+                i += bestLen - 1;
+                onlyFail = 0;
+                hadMatch = 1;
             }
         }
         if (hadMatch) {
