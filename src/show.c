706f0af79e4788638326edbb5411c7bf8be63eef
usr-undeleted <zephimhere@gmail.com>
2026-05-13 14:46:12 -0300

    new flags for showing hidden chars in show.c

--- src/show.c
+++ src/show.c
@@ -6,6 +6,11 @@
 #include <fcntl.h>
 #include <errno.h>
 
+// invisible chars
+unsigned int nonPrinting = 0;
+unsigned int eofChar = 0;
+unsigned int tabChar = 0;
+
 void helpMenu(char *invocation) {
     printf("\e[1m%s\e[0m command basic usage:\n"
         "   \e[1m%s\e[0m <flags> <file(s)>\n"
@@ -13,11 +18,59 @@ void helpMenu(char *invocation) {
         "   use '%s -' to specificy stdin from -, as long as it is provided.\n"
         "\e[1m%s\e[0m will print the contents all files you specify.\n\n"
         "flags:\n"
-        "   \e[1m-h\e[0m or \e[1m--help\e[0m: show this menu.\n\n"
+        "   \e[1m-h\e[0m or \e[1m--help\e[0m: show this menu.\n"
+        "   \e[1m-v\e[0m or \e[1m--show-nonprinting\e[0m: use ^ and M- for hidden chars not normally printed.\n"
+        "   \e[1m-T\e[0m or \e[1m--show-tabs\e[0m: show tab characters as ^I.\n"
+        "   \e[1m-E\e[0m or \e[1m--show-eof\e[0m: show EOFs as $.\n"
         "\e[2;3m%s is part of the cutie project hosted under https://github.com/usr-undeleted/cutie licensed under the GPLv3 license.\e[0m\n",
         invocation, invocation, invocation, invocation, invocation
     );
     exit(0);
+}
+
+// called by dump() to handle invisible stuff n all
+int dumpVisible(FILE *fd) {
+    char buf[4096];
+    size_t n;
+    int returned = 0;
+
+    while ((n = fread(buf, 1, sizeof(buf), fd)) > 0) {
+        for (size_t i = 0; i < n; i++) {
+            unsigned char ch = buf[i];
+
+            if (eofChar && ch == '\n') {
+                printf("$\n");
+                continue;
+            }
+
+            if (tabChar && ch == '\t') {
+                printf("^I");
+                continue;
+            }
+
+            if (nonPrinting) {
+                if (ch <= 31 && ch >= 0 && ch != '\n' && ch != '\t') {
+                    printf("^%c", ch + '@');
+                    continue;
+                }
+
+                if (ch == 127) {
+                    printf("^?");
+                    continue;
+                }
+
+                if (ch <= 255 && ch >= 128) {
+                    printf("M-%c", ch - 128);
+                    continue;
+                }
+            }
+
+            // regular printing
+            putchar(ch);
+        }
+    }
+
+    return returned;
 }
 
 // return 1 on fail
@@ -25,6 +78,8 @@ int dump(FILE *fd) {
     char buf[4096];
     size_t n;
     int returned = 0;
+
+    if (nonPrinting || eofChar || tabChar) return dumpVisible(fd);
 
     // dump
     while ((n = fread(buf, 1, sizeof(buf), fd)) > 0) {
@@ -47,6 +102,45 @@ int main (int argc, char *argv[]) {
     unsigned int processStdin = 0;
     // return code
     int returned = 0;
+
+    // manage flags
+    char charFlags[] = {
+        'h',
+        'v',
+        'T',
+        'E'
+    };
+    char *stringFlags[] = {
+        "--help",
+        "--show-nonprinting"
+        "--show-tabs",
+        "--show-eof"
+    };
+    int charLen = sizeof(charFlags) / sizeof(charFlags[0]);
+    int stringLen = sizeof(stringFlags) / sizeof(stringFlags[0]);
+    struct flagInput input = {
+        charFlags, charLen,
+        stringFlags, stringLen,
+        0
+    };
+
+    int *flags = labelFlags(argc, argv, &input);
+
+    if (flags != NULL) {
+        for (int i = 0; i < input.flagCount; i++) {
+            if (flags[i] == 0) helpMenu(argv[0]);
+
+            if (flags[i] == 1) nonPrinting = 1;
+
+            if (flags[i] == 2) tabChar = 1;
+
+            if (flags[i] == 3) eofChar = 1;
+        }
+    } else {
+        fprintf(stderr, "Invalid flag detected. See '%s -h' or '%s --help' for instructions.\n", argv[0], argv[0]);
+        return 2;
+    }
+    free(flags);
 
     // do we process stdin?
     if (!isatty(STDIN_FILENO)) {
