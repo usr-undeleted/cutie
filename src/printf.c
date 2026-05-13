02d58c467203cbe4af8b942d7fac84c6d94e2e83
usr-undeleted <zephimhere@gmail.com>
2026-05-13 15:37:29 -0300

    more escape characters, separate printing into func, handle stdin

--- src/printf.c
+++ src/printf.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    if (argc <= 1) {
-        return 0;
-    }
-    for (char *p = argv[1]; *p; p++) {
+void print(char *str) {
+    for (char *p = str; *p; p++) {
         if (*p == '\\' && *(p+1)) {
             p++;
             switch (*p) {
                 case 'n': putchar('\n'); break;
                 case 't': putchar('\t'); break;
                 case 'e': putchar('\e'); break;
+                case 'a': putchar('\a'); break;
+                case 'r': putchar('\r'); break;
+                case 'v': putchar('\v'); break;
+                case 'f': putchar('\f'); break;
+                case '"': putchar('"'); break;
+                case 'c': exit(0);
 
                 case '0': case '1': case '2': case '3':
                 case '4': case '5': case '6': case '7': {
@@ -28,6 +33,30 @@ int main(int argc, char *argv[]) {
             putchar(*p);
         }
     }
+}
+
+int main(int argc, char *argv[]) {
+    unsigned int piped = 0;
+    if (!isatty(STDIN_FILENO)) piped = 1;
+
+    if (argc <= 1 && !piped) {
+        printf("Not enough arguments given. See '%s -h' or '%s --help' for instructions.\n", argv[0], argv[0]);
+        return 2;
+    }
+
+    if (piped) {
+        char buf[4096];
+        size_t n;
+
+        while ((n = fread(buf, 1, sizeof(buf), stdin)) > 0) {
+            buf[n] = '\0';
+            print(buf);
+        }
+
+        if (argc == 1) return 0;
+    }
+
+    print(argv[1]);
 
     return 0;
 }
